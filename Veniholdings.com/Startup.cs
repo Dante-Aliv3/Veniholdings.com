3cdf1769fdab4c63a912718b2726d57db36b0cd9
Holston <devante.holston@americanbuildings.com>
2018-06-29 16:03:18 -0500

    Add project files.

---
+++ Veniholdings.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Veniholdings.com.Startup))]
+namespace Veniholdings.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
